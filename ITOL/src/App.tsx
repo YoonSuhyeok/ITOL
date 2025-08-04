b4c3af1943f3c0a3eedc13f977f50685d16f2339
Yoon Suhyeok <sh.yoon13@konai.com>
2025-08-04 21:59:09 +0900

    refactor: improve type annotations and optimize nodeTypes definition in App component

--- ITOL/src/App.tsx
+++ ITOL/src/App.tsx
@@ -1,13 +1,11 @@
-import { Background, ReactFlow, Node, useNodesState, useEdgesState, addEdge, MarkerType } from "@xyflow/react";
+import { Background, ReactFlow, useNodesState, useEdgesState, addEdge, MarkerType, Connection } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
-import fileNode from "@/entities/language/ui/file-node";
 
 import "./App.css";
 import { DagServiceInstance } from "./features/dag/services/dag.service";
-import type FileNodeData from "@/entities/language/model/file-type";
 import FileNode from "@/entities/language/ui/file-node";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 
 export default function App() {
 
@@ -15,13 +13,16 @@ export default function App() {
 	// const edges = DagServiceInstance.getEdgeData();
 	const [nodes, setNodes, onNodesChange] = useNodesState(DagServiceInstance.getNodeData());
 
-	const nodeTypes = {
-		languageNode: (nodeProps: any) => <FileNode {...nodeProps} setNodes={setNodes} />
-	};
+	const nodeTypes = useMemo(
+		() => ({
+			languageNode: (nodeProps: any) => <FileNode {...nodeProps} setNodes={setNodes} />
+		}),
+		[setNodes]
+	);
 
 	const [edges, setEdges, onEdgesChange] = useEdgesState(DagServiceInstance.getEdgeData());
 	const onConnect = useCallback(
-		(connection) => {
+		(connection: Connection) => {
 		  setEdges((oldEdges) => {
 			const newEdges = addEdge(
 			  {
